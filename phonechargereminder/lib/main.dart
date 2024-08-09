873dd1d203e47c1b9cdb42e0726db81e50021261
ryan <rom@ualberta.ca>
2024-08-09 01:36:51 -0600

    show battery level into screen

--- phonechargereminder/lib/main.dart
+++ phonechargereminder/lib/main.dart
@@ -1,18 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:battery_plus/battery_plus.dart';
 
+// Instantiate it
+var battery = Battery();
+int batteryLevel = 0;
+
+// Import package
 void main() {
-  runApp(const MainApp());
+  // Access current battery level
+  runApp(MainApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+
+class MainApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MainApp();
+}
+
+class _MainApp extends State<MainApp>{
+
+  @override
+  void initState() {
+    super.initState();
+    battery.batteryLevel.then((value) => {
+      batteryLevel = value,
+      setState((){
+        batteryLevel;
+      })
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text('Battery Percentage : $batteryLevel'),
         ),
       ),
     );
